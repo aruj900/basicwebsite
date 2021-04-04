6747f3ebcb861a1e0b58879701b3698f54ebc4f0
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-04-04 12:17:48 +0530

    api

--- API/WeatherProject/app.js
+++ API/WeatherProject/app.js
@@ -15,16 +15,12 @@ app.post("/", function(req, res){
   const query = req.body.cityName
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=metric&appid=2867bc286fef728c1b093cfd0743a36f";
   https.get(url,function(response){
-    console.log(response.statusCode);
     response.on("data", function(data){
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
       const temp = weatherData.main.temp;
-      console.log(temp);
       const weatherDiscription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/" +  icon + "@2x.png";
-      console.log(weatherDiscription);
       res.write("<p>The weather is currently " + weatherDiscription + "</p>");
       res.write("<h1>The Temperature in " + query + " is " + temp + " degrees celcius.</h1>");
       res.write("<img src=" + imageURL + ">");
