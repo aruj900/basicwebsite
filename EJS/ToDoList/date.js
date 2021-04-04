e554223c75ec7e3cd26f4f45b4df686adca98c16
Aruj Mahajan <arujmahajan@Arujs-MacBook-Pro.local>
2021-04-04 20:52:38 +0530

    EJS

---
+++ EJS/ToDoList/date.js
@@ -0,0 +1,13 @@
+exports.getDate = getDate;
+
+function getDate() {
+  var today = new Date();
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+
+}
